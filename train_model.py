04478f74f2e6aeeda7029daea2acdda86bbfa09c
supr1yo <supriyo.developer@gmail.com>
2025-05-25 14:34:47 +0530

    feat: show precision score

--- train_model.py
+++ train_model.py
@@ -3,26 +3,22 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score
 import joblib
 
-# Load the dataset
+# Load dataset
 df = pd.read_csv('./data/spam.csv', encoding='latin-1')[['v1', 'v2']]
-df.columns = ['label', 'message'] # Rename the columns
+df.columns = ['label', 'message']  # Rename columns
 
-# Assign columns to vars
-label = df['label']
+# Convert categorical labels to numeric values
+df['label'] = df['label'].map({'ham': 0, 'spam': 1})
+
+# Assign features and labels
 message = df['message']
-
-# Convert categorical labels to numeric value
-df['label'] = df['label'].map({
-    'ham': 0,
-    'spam': 1
-})
+label = df['label']
 
 # Split data
 x_train, x_test, y_train, y_test = train_test_split(message, label, test_size=0.2, random_state=42)
-
 
 # Pipeline: Vectorizer + Classifier
 model = Pipeline([
@@ -33,9 +29,10 @@ model = Pipeline([
 # Train model
 model.fit(x_train, y_train)
 
-
 # Save model
 joblib.dump(model, './models/model.pkl')
 
-# Print accuracy
-print(f"Accuracy: {accuracy_score(y_test, model.predict(x_test)):.2f}")
+# Evaluate model
+y_pred = model.predict(x_test)
+print(f"Accuracy: {accuracy_score(y_test, y_pred):.2f}")
+print(f"Precision: {precision_score(y_test, y_pred):.2f}")
